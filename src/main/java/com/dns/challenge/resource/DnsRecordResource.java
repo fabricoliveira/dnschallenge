829ee52ba377c66c9985e2995b47e63009c3c57f
Fabricio dos Santos Oliveira <43307831+fabricoliveira@users.noreply.github.com>
2020-04-15 00:38:25 -0300

    Implementing comunication between the endpoint and service class

--- src/main/java/com/dns/challenge/resource/DnsRecordResource.java
+++ src/main/java/com/dns/challenge/resource/DnsRecordResource.java
@@ -1,24 +1,31 @@
 package com.dns.challenge.resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.dns.challenge.service.DnsRecordService;
 
 @RestController
 @RequestMapping
 public class DnsRecordResource {
 
 	
+	@Autowired
+	private DnsRecordService dnsRecordService;
+	
+	
 	@PostMapping(consumes = "application/json")
 	public String save() {
-		return "SAVE test";
+		return dnsRecordService.save();
 	}
 	
 	
 	@GetMapping(consumes = "application/json")
 	public String find() {
-		return "FIND test";
+		return dnsRecordService.find();
 	}
 	
 	
