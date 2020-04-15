871ca96feba469e27c7178e2ea1e2e305ac334ce
Fabricio dos Santos Oliveira <43307831+fabricoliveira@users.noreply.github.com>
2020-04-14 21:36:05 -0300

    First Commit

---
+++ src/main/java/com/dns/challenge/DnschallengeApplication.java
@@ -0,0 +1,13 @@
+package com.dns.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnschallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnschallengeApplication.class, args);
+	}
+
+}
