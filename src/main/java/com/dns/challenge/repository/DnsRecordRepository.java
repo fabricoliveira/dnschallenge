e698761665d6ed4b70c5e535f5c2894ff4385c70
Fabricio dos Santos Oliveira <43307831+fabricoliveira@users.noreply.github.com>
2020-04-15 01:13:54 -0300

    Commiting the query needed to find 'all dns records and ip by hostnames' and the query to 'map hostnames with mathing dns records by selected hostnames'

--- src/main/java/com/dns/challenge/repository/DnsRecordRepository.java
+++ src/main/java/com/dns/challenge/repository/DnsRecordRepository.java
@@ -1,9 +1,32 @@
 package com.dns.challenge.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.dns.challenge.model.DnsRecord;
 
 public interface DnsRecordRepository extends JpaRepository<DnsRecord, Long> {
+
+	
+	@Query(value = "SELECT hn.dnsRecordId as id, ip.address as address, COUNT(*) as count "
+				 + "FROM hostname hn "
+				 + "INNER JOIN ip ip "
+			     + "ON hn.dnsRecordId = ip.dnsRecordId "
+			     + "WHERE hn.name in ?1 "
+			     + "AND hn.dnsRecordId NOT IN (SELECT dnsRecordId FROM hostname hn WHERE hn.name in ?2) "
+			     + "GROUP BY hn.dnsRecordId, ip.address "
+			     + "HAVING COUNT(*) > ?3", nativeQuery = true)
+	Object[][] findAllDnsRecordAndIpByHostnames(List<String> included, List<String> excluded, int counter);
+	
+	
+	
+	@Query(value = "SELECT name, count(*) "
+				 + "FROM hostname "
+				 + "WHERE dnsRecordId in ?1 "
+				 + "AND name not in ?2 "
+				 + "group by name", nativeQuery = true)
+	Object[][] mapHostnamesWithDnsRecordsBySelectedHostnames(List<String> dnsRecordIds, List<String> excluded);
 
 }
