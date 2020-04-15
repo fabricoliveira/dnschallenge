8b4201ad2f931fb56e88da6fbf03682042fe3af7
Fabricio dos Santos Oliveira <43307831+fabricoliveira@users.noreply.github.com>
2020-04-15 01:29:33 -0300

    Updating the save and find endpoints to work with Service Class

--- src/main/java/com/dns/challenge/resource/DnsRecordResource.java
+++ src/main/java/com/dns/challenge/resource/DnsRecordResource.java
@@ -1,11 +1,22 @@
 package com.dns.challenge.resource;
 
+import java.net.URI;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.dns.challenge.model.DnsRecord;
+import com.dns.challenge.model.dto.DnsRecordDTO;
+import com.dns.challenge.model.dto.ResultDTO;
+import com.dns.challenge.model.dto.SearchDTO;
 import com.dns.challenge.service.DnsRecordService;
 
 @RestController
@@ -18,14 +29,33 @@ public class DnsRecordResource {
 	
 	
 	@PostMapping(consumes = "application/json")
-	public String save() {
-		return dnsRecordService.save();
+	public ResponseEntity<DnsRecordDTO> save(@RequestBody DnsRecordDTO dnsRecordDTO, HttpServletResponse response) throws Exception {
+		
+		DnsRecord dnsRecordStored = dnsRecordService.save(dnsRecordDTO);
+		
+		dnsRecordDTO.setId(dnsRecordStored.getId());
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+											 .path("/{id}")	
+								   			 .buildAndExpand(dnsRecordStored.getId())
+								   			 .toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(dnsRecordDTO);
 	}
 	
 	
 	@GetMapping(consumes = "application/json")
-	public String find() {
-		return dnsRecordService.find();
+	public ResponseEntity<ResultDTO> find(@RequestBody SearchDTO searchDTO) throws Exception {
+		
+		ResultDTO result = dnsRecordService.find(searchDTO);
+		
+		if(result != null) {
+			
+			return ResponseEntity.ok(result);
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 	
 	
